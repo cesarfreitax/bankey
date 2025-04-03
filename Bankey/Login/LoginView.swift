cf7d1efc17c5b9fa3eac6d2b9951ac8e8213ed00
cbento <cesar.bento@sicpa.com>
2025-04-03 09:02:13 -0300

    chore: login feature

---
+++ Bankey/Login/LoginView.swift
@@ -0,0 +1,86 @@
+//
+//  LoginView.swift
+//  Bankey
+//
+//  Created by Bento, Cesar on 31/03/25.
+//
+
+import Foundation
+import UIKit
+
+class LoginView: UIView {
+    let loginStackView = UIStackView()
+    let usernameTextField = UITextField()
+    let divider = UIView()
+    let passwordTextField = UITextField()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupStyle()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    override var intrinsicContentSize: CGSize {
+//        return .init(width: 300, height: 400)
+//    }
+}
+
+extension LoginView {
+    private func setupStyle() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .secondarySystemBackground
+        layer.cornerRadius = 5
+        
+        loginStackView.translatesAutoresizingMaskIntoConstraints = false
+        loginStackView.axis = .vertical
+        loginStackView.spacing = 1
+        
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.placeholder = "Username"
+        usernameTextField.delegate = self
+        
+        divider.translatesAutoresizingMaskIntoConstraints = false
+        divider.backgroundColor = .secondarySystemFill
+        
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.delegate = self
+    }
+    
+    private func setupLayout() {
+        loginStackView.addArrangedSubview(usernameTextField)
+        loginStackView.addArrangedSubview(divider)
+        loginStackView.addArrangedSubview(passwordTextField)
+        addSubview(loginStackView)
+        
+        NSLayoutConstraint.activate([
+            loginStackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            loginStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: loginStackView.trailingAnchor, multiplier: 1),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: loginStackView.bottomAnchor, multiplier: 1)
+        ])
+        
+        divider.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+}
+
+extension LoginView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+
+    }
+}
---
+++ Bankey/Login/LoginViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Bento, Cesar on 28/03/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    let loginView = LoginView()
+    let signInButton = UIButton()
+    let errorText = UILabel()
+    
+    var usernameText: String? {
+        loginView.usernameTextField.text
+    }
+    
+    var passwordText: String? {
+        loginView.passwordTextField.text
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupStyle()
+        setupLayout()
+    }
+}
+
+extension LoginViewController {
+    func setupStyle() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.setTitle("Sign In", for: .normal)
+        signInButton.configuration = .filled()
+        signInButton.addTarget(self, action: #selector(handleSignIn), for: .touchUpInside)
+        
+        errorText.translatesAutoresizingMaskIntoConstraints = false
+        errorText.textColor = .systemRed
+        errorText.textAlignment = .center
+        errorText.numberOfLines = 0
+        errorText.isHidden = true
+        errorText.text = "Invalid email or password"
+    }
+
+
+    func setupLayout() {
+        view.addSubview(loginView)
+        view.addSubview(signInButton)
+        view.addSubview(errorText)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1),
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+            errorText.topAnchor.constraint(equalToSystemSpacingBelow: signInButton.bottomAnchor, multiplier: 1),
+            errorText.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            errorText.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
+    }
+}
+
+extension LoginViewController {
+    @objc func handleSignIn() {
+        errorText.isHidden = true
+        
+        guard let username = usernameText, let password = passwordText else {
+            showErrorMessage(withMessage: "Username / password can not be nil!")
+            return
+        }
+        
+        if username.isEmpty || password.isEmpty {
+            showErrorMessage(withMessage: "Username / password can not be blank!")
+            return
+        }
+        
+        if username == "cesar" && password == "12345" {
+            signInButton.configuration?.showsActivityIndicator = true
+            signInButton.configuration?.imagePadding = 6
+            
+        } else {
+            showErrorMessage(withMessage: "Username / password incorrect!")
+        }
+        
+        return
+    }
+    
+    private func showErrorMessage(withMessage message: String) {
+        errorText.text = message
+        errorText.isHidden = false
+    }
+}
